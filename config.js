32118a3a19b6acbe5b0ad8cf9bd3adf81a10c1d6
Jumpmam <pacman228pac@gmail.com>
2026-09-07 06:06:26 +0300

    Buy link points to the Pons Family launchpad

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Public config. Flip these when the token is live.
-export const CONFIG = { ticker: "$C4T", ca: "SOON", pump: "", x: "https://x.com/0xc4cat", dexscreener: "", url: "https://c4t-site.vercel.app" };
+export const CONFIG = { ticker: "$C4T", ca: "SOON", pump: "https://www.ponsfamily.com/launchpad", x: "https://x.com/0xc4cat", dexscreener: "", url: "https://c4t-site.vercel.app" };
