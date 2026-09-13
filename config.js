298536474d5910886e7ca4d6c9babb5f919fe1e7
Jumpmam <pacman228pac@gmail.com>
2026-09-13 03:55:34 +0300

    config: new contract, stonkfun buy, new X account

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Public config. Flip these when the token is live.
-export const CONFIG = { ticker: "$C4T", ca: "0x44ee933bf91e6271b673c336606fdb10dcf89186", pump: "https://www.ponsfamily.com/launchpad/0x44ee933bf91e6271b673c336606fdb10dcf89186", x: "https://x.com/0xc4cat", dexscreener: "", url: "https://c4t-site.vercel.app" };
+export const CONFIG = { ticker: "$C4T", ca: "2koWVkLKSF25y18KRMHA6MnmtrKLohmaNjVUdojQui1a", pump: "https://www.stonkfun.xyz/", x: "https://x.com/c4cat_pf", dexscreener: "", url: "https://c4t-site.vercel.app" };
