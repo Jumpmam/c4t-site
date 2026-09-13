49d8dfccb37bca81aeeb7e05cee30132144ac99b
Jumpmam <pacman228pac@gmail.com>
2026-09-13 04:03:43 +0300

    config: new contract

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Public config. Flip these when the token is live.
-export const CONFIG = { ticker: "$C4T", ca: "2koWVkLKSF25y18KRMHA6MnmtrKLohmaNjVUdojQui1a", pump: "https://www.stonkfun.xyz/", x: "https://x.com/c4cat_pf", dexscreener: "", url: "https://c4t-site.vercel.app" };
+export const CONFIG = { ticker: "$C4T", ca: "5RCa6aUuKcTsnx4aMiQSU7PdjuzV3GuyzcRCQ41Jhp4V", pump: "https://www.stonkfun.xyz/", x: "https://x.com/c4cat_pf", dexscreener: "", url: "https://c4t-site.vercel.app" };
